11c8e81065e90c250beb2801c78ba381836daa78
Rishu Kashyap <rishusweta07@gmail.com>
2024-03-20 16:03:53 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+function page() {
+  return (
+    <p>Customers page</p>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+function page() {
+  return (
+    <div>Invoices page</div>
+  )
+}
+
+export default page
